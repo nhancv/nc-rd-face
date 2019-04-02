e4bacd3025deca01643b5aa88bc762b4694c4d5b
Nhan Cao <nhancv92@gmail.com>
2019-04-02 23:17:47 +0700

    initial

---
+++ facemask/facemask/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  facemask
+//
+//  Created by iNhan Cao on 4/2/19.
+//  Copyright © 2019 iNhan Cao. All rights reserved.
+//
+
+#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace std;
+using namespace cv;
+int main()
+{
+    Mat img = imread("lena.jpg");
+    namedWindow("image", WINDOW_NORMAL);
+    imshow("image", img);
+    waitKey(0);
+    return 0;
+}
