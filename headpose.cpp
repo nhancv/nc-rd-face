e4778b0c09c552b1135db2842a742166c535b834
Nhan Cao <nhancv92@gmail.com>
2019-04-04 23:14:47 +0700

    traking

--- headpose.cpp
+++ headpose.cpp
@@ -1,69 +1,104 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/tracking.hpp>
+#include <opencv2/tracking/tracker.hpp>
+#include <opencv2/tracking/tldDataset.hpp>
+#include <opencv2/core/ocl.hpp>
 
-using namespace std;
 using namespace cv;
+using namespace std;
 
 int main(int argc, char **argv) {
+    // List of tracker types in OpenCV 3.4.1
+    string trackerTypes[8] = {"BOOSTING", "MIL", "KCF", "TLD", "MEDIANFLOW", "GOTURN", "MOSSE", "CSRT"};
+    // vector <string> trackerTypes(types, std::end(types));
 
-    // Read input image
-    cv::Mat im = cv::imread("headPose.jpg");
+    // Create a tracker
+    string trackerType = trackerTypes[2];
 
-    // 2D image points. If you change the image, you need to change vector
-    std::vector<cv::Point2d> image_points;
-    image_points.push_back(cv::Point2d(359, 391));    // Nose tip
-    image_points.push_back(cv::Point2d(399, 561));    // Chin
-    image_points.push_back(cv::Point2d(337, 297));     // Left eye left corner
-    image_points.push_back(cv::Point2d(513, 301));    // Right eye right corner
-    image_points.push_back(cv::Point2d(345, 465));    // Left Mouth corner
-    image_points.push_back(cv::Point2d(453, 469));    // Right mouth corner
+    Ptr<Tracker> tracker;
 
-    // 3D model points.
-    std::vector<cv::Point3d> model_points;
-    model_points.push_back(cv::Point3d(0.0f, 0.0f, 0.0f));               // Nose tip
-    model_points.push_back(cv::Point3d(0.0f, -330.0f, -65.0f));          // Chin
-    model_points.push_back(cv::Point3d(-225.0f, 170.0f, -135.0f));       // Left eye left corner
-    model_points.push_back(cv::Point3d(225.0f, 170.0f, -135.0f));        // Right eye right corner
-    model_points.push_back(cv::Point3d(-150.0f, -150.0f, -125.0f));      // Left Mouth corner
-    model_points.push_back(cv::Point3d(150.0f, -150.0f, -125.0f));       // Right mouth corner
+#if (CV_VERSION_MAJOR < 3)
+    {
+        tracker = Tracker::create(trackerType);
+    }
+#else
+    {
+        if (trackerType == "BOOSTING")
+            tracker = TrackerBoosting::create();
+        if (trackerType == "MIL")
+            tracker = TrackerMIL::create();
+        if (trackerType == "KCF")
+            tracker = TrackerKCF::create();
+        if (trackerType == "TLD")
+            tracker = TrackerTLD::create();
+        if (trackerType == "MEDIANFLOW")
+            tracker = TrackerMedianFlow::create();
+        if (trackerType == "GOTURN")
+            tracker = TrackerGOTURN::create();
+        if (trackerType == "MOSSE")
+            tracker = TrackerMOSSE::create();
+        if (trackerType == "CSRT")
+            tracker = TrackerCSRT::create();
+    }
+#endif
+    // Read video
+    VideoCapture video("videos/chaplin.mp4");
 
-    // Camera internals
-    double focal_length = im.cols; // Approximate focal length.
-    Point2d center = cv::Point2d(im.cols / 2, im.rows / 2);
-    cv::Mat camera_matrix = (cv::Mat_<double>(3, 3) << focal_length, 0, center.x, 0, focal_length, center.y, 0, 0, 1);
-    cv::Mat dist_coeffs = cv::Mat::zeros(4, 1, cv::DataType<double>::type); // Assuming no lens distortion
-
-    cout << "Camera Matrix " << endl << camera_matrix << endl;
-    // Output rotation and translation
-    cv::Mat rotation_vector; // Rotation in axis-angle form
-    cv::Mat translation_vector;
-
-    // Solve for pose
-    cv::solvePnP(model_points, image_points, camera_matrix, dist_coeffs, rotation_vector, translation_vector);
-
-
-    // Project a 3D point (0, 0, 1000.0) onto the image plane.
-    // We use this to draw a line sticking out of the nose
-
-    vector<Point3d> nose_end_point3D;
-    vector<Point2d> nose_end_point2D;
-    nose_end_point3D.push_back(Point3d(0, 0, 1000.0));
-
-    projectPoints(nose_end_point3D, rotation_vector, translation_vector, camera_matrix, dist_coeffs, nose_end_point2D);
-
-
-    for (int i = 0; i < image_points.size(); i++) {
-        circle(im, image_points[i], 3, Scalar(0, 0, 255), -1);
+    // Exit if video is not opened
+    if (!video.isOpened()) {
+        cout << "Could not read video file" << endl;
+        return 1;
     }
 
-    cv::line(im, image_points[0], nose_end_point2D[0], cv::Scalar(255, 0, 0), 2);
+    // Read first frame
+    Mat frame;
+    bool ok = video.read(frame);
 
-    cout << "Rotation Vector " << endl << rotation_vector << endl;
-    cout << "Translation Vector" << endl << translation_vector << endl;
+    // Define initial bounding box
+    Rect2d bbox(287, 23, 86, 320);
 
-    cout << nose_end_point2D << endl;
+    // Uncomment the line below to select a different bounding box
+    // bbox = selectROI(frame, false);
+    // Display bounding box.
+    rectangle(frame, bbox, Scalar(255, 0, 0), 2, 1);
 
-    // Display image.
-    cv::imshow("Output", im);
-    cv::waitKey(0);
+    imshow("Tracking", frame);
+    tracker->init(frame, bbox);
 
+    while (video.read(frame)) {
+        // Start timer
+        double timer = (double) getTickCount();
+
+        // Update the tracking result
+        bool ok = tracker->update(frame, bbox);
+
+        // Calculate Frames per second (FPS)
+        float fps = getTickFrequency() / ((double) getTickCount() - timer);
+
+        if (ok) {
+            // Tracking success : Draw the tracked object
+            rectangle(frame, bbox, Scalar(255, 0, 0), 2, 1);
+        } else {
+            // Tracking failure detected.
+            putText(frame, "Tracking failure detected", Point(100, 80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 255),
+                    2);
+        }
+
+        // Display tracker type on frame
+        putText(frame, trackerType + " Tracker", Point(100, 20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50, 170, 50), 2);
+
+        // Display FPS on frame
+        putText(frame, "FPS : " + to_string(int(fps)), Point(100, 50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50, 170, 50),
+                2);
+
+        // Display frame.
+        imshow("Tracking", frame);
+
+        // Exit if ESC pressed.
+        int k = waitKey(1);
+        if (k == 27) {
+            break;
+        }
+
+    }
 }
