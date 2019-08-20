56bf84a6c6bb690d42b3a5fa3377156e84f5ed5b
Nhan Cao <nhancv92@gmail.com>
2019-08-20 16:18:10 +0700

    'update'

--- vision.cpp
+++ vision.cpp
@@ -102,10 +102,12 @@ void fourPointTransform(const Mat &src, Mat &dst, vector<Point> pts) {
 void preProcess(const Mat &src, Mat &dst) {
     Mat imageGrayed;
     Mat imageOpen, imageClosed, imageBlurred;
-
-    cvtColor(src, imageGrayed, CV_BGR2GRAY);
-//    imshow("Image grayed", imageGrayed);
-
+    if (src.channels() == 3) {
+        cvtColor(src, imageGrayed, CV_BGR2GRAY);
+    } else {
+        imageGrayed = src;
+    }
+    imshow("Image grayed", imageGrayed);
     Mat structuringElmt = getStructuringElement(MORPH_ELLIPSE, Size(4, 4));
     morphologyEx(imageGrayed, imageOpen, MORPH_OPEN, structuringElmt);
     morphologyEx(imageOpen, imageClosed, MORPH_CLOSE, structuringElmt);
@@ -114,10 +116,200 @@ void preProcess(const Mat &src, Mat &dst) {
 
     GaussianBlur(imageClosed, imageBlurred, Size(1, 1), 0);
     Canny(imageBlurred, dst, 75, 200);
-//    imshow("Image imageBlurred", imageBlurred);
-//    imshow("Image dst", dst);
+    imshow("Image imageBlurred", imageBlurred);
+    imshow("Image dst", dst);
 
 
+}
+
+Mat linesDetection(const Mat &src) {
+    // Transform source image to gray if it is not
+    Mat gray;
+    if (src.channels() == 3) {
+        cvtColor(src, gray, CV_BGR2GRAY);
+    } else {
+        gray = src;
+    }
+    // Show gray image
+//    imshow("gray", gray);
+    // Apply adaptiveThreshold at the bitwise_not of gray, notice the ~ symbol
+    Mat bw;
+    adaptiveThreshold(~gray, bw, 255, CV_ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 9, -2);
+
+    // Show binary image
+//    imshow("binary", bw);
+    // Create the images that will use to extract the horizontal and vertical lines
+    Mat horizontal = bw.clone();
+    Mat vertical = bw.clone();
+    // Specify size on horizontal axis
+    int horizontalsize = horizontal.cols / 50;
+    // Create structure element for extracting horizontal lines through morphology operations
+    Mat horizontalStructure = getStructuringElement(MORPH_RECT, Size(horizontalsize, 1));
+    // Apply morphology operations
+    erode(horizontal, horizontal, horizontalStructure, Point(-1, -1));
+    dilate(horizontal, horizontal, horizontalStructure, Point(-1, -1));
+
+    // Connect nearest broken line
+    Mat horizontalStructureConnect = getStructuringElement(MORPH_RECT, Size(5, 1));
+    dilate(horizontal, horizontal, horizontalStructureConnect, Point(-1, -1));
+    // Remove small line
+    Mat horizontalStructureShort = getStructuringElement(MORPH_RECT, Size(5, 1));
+    erode(horizontal, horizontal, horizontalStructureShort, Point(-1, -1));
+    // Connect nearest broken line
+    dilate(horizontal, horizontal, horizontalStructureConnect, Point(-1, -1));
+    dilate(horizontal, horizontal, horizontalStructureConnect, Point(-1, -1));
+    dilate(horizontal, horizontal, horizontalStructureConnect, Point(-1, -1));
+    // Show extracted horizontal lines
+//    imshow("horizontal", horizontal);
+
+
+    // Specify size on vertical axis
+    int verticalsize = vertical.rows / 30;
+    // Create structure element for extracting vertical lines through morphology operations
+    Mat verticalStructure = getStructuringElement(MORPH_RECT, Size(1, verticalsize));
+    // Apply morphology operations
+    erode(vertical, vertical, verticalStructure, Point(-1, -1));
+    dilate(vertical, vertical, verticalStructure, Point(-1, -1));
+
+    // Connect nearest broken line
+    Mat verticalStructureConnect = getStructuringElement(MORPH_RECT, Size(1, 5));
+    dilate(vertical, vertical, verticalStructureConnect, Point(-1, -1));
+    // Remove small line
+    Mat verticalStructureShort = getStructuringElement(MORPH_RECT, Size(1, 5));
+    erode(vertical, vertical, verticalStructureShort, Point(-1, -1));
+    // Connect nearest broken line
+    dilate(vertical, vertical, verticalStructureConnect, Point(-1, -1));
+    dilate(vertical, vertical, verticalStructureConnect, Point(-1, -1));
+    dilate(vertical, vertical, verticalStructureConnect, Point(-1, -1));
+
+    // Show extracted vertical lines
+//    imshow("vertical", vertical);
+
+    Mat mask = vertical + horizontal;
+//    imshow("mask", mask);
+    vertical = mask;
+
+
+    // Inverse vertical image
+//    bitwise_not(vertical, vertical);
+//    imshow("vertical_bit", vertical);
+    // Extract edges and smooth image according to the logic
+    // 1. extract edges
+    // 2. dilate(edges)
+    // 3. src.copyTo(smooth)
+    // 4. blur smooth img
+    // 5. smooth.copyTo(src, edges)
+    // Step 1
+    Mat edges;
+    adaptiveThreshold(vertical, edges, 255, CV_ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 3, -2);
+//    imshow("edges", edges);
+    // Step 2
+    Mat kernel = Mat::ones(2, 2, CV_8UC1);
+    dilate(edges, edges, kernel);
+//    imshow("dilate", edges);
+    // Step 3
+    Mat smooth;
+    vertical.copyTo(smooth);
+    // Step 4
+    blur(smooth, smooth, Size(2, 2));
+    // Step 5
+    smooth.copyTo(vertical, edges);
+    // Show final result
+//    imshow("smooth", vertical);
+
+    /**
+     * Use HoughLinesP to get all posible lines and find 4 limit points min-top-left, max-bottom-right
+     * Depend on 4 limit points, get center of image, combine with valid lines to fine correct left, top, right, bottom.
+     */
+    vector<Vec4i> lines;
+    Mat color = src.clone();
+    Mat out = Mat::zeros(mask.size(), mask.type());
+    HoughLinesP(vertical, lines, 1, CV_PI / 180, 80, 60, 10);
+    int xMin = mask.cols;
+    int yMin = mask.rows;
+    int xMax = 0;
+    int yMax = 0;
+    for (size_t i = 0; i < lines.size(); i++) {
+        Vec4i l = lines[i];
+        Point a(l[0], l[1]);
+        Point b(l[2], l[3]);
+        double res = cv::norm(a - b);
+        if (res > mask.cols / 2.0) {
+            xMin = cv::min(xMin, a.x);
+            xMax = cv::max(xMax, b.x);
+
+            line(color, a, b, Scalar(255));
+            circle(color, a, 5, Scalar(0, 0, 255) /*Red*/, 2);
+            circle(color, b, 5, Scalar(0, 255, 255) /*Yellow*/, 2);
+        }
+
+        if (res > mask.rows / 2.0) {
+            yMin = cv::min(yMin, a.y);
+            yMax = cv::max(yMax, b.y);
+
+            line(color, a, b, Scalar(255));
+            circle(color, a, 5, Scalar(0, 0, 255) /*Red*/, 2);
+            circle(color, b, 5, Scalar(0, 255, 255) /*Yellow*/, 2);
+        }
+    }
+
+    int middleX = (xMax + xMin) / 2;
+    int middleY = (yMax + yMin) / 2;
+    Point leftTop(middleX, middleY);
+    Point topRight(middleX, middleY);
+    Point rightBottom(middleX, middleY);
+    Point bottomLeft(middleX, middleY);
+    for (size_t i = 0; i < lines.size(); i++) {
+        Vec4i l = lines[i];
+        Point a(l[0], l[1]);
+        Point b(l[2], l[3]);
+        // Find left, top
+        if (a.x < middleX && a.y < middleY) {
+            leftTop.x = cv::min(a.x, leftTop.x);
+            leftTop.y = cv::min(a.y, leftTop.y);
+        }
+        if (b.x < middleX && b.y < middleY) {
+            leftTop.x = cv::min(b.x, leftTop.x);
+            leftTop.y = cv::min(b.y, leftTop.y);
+        }
+        // Find top, right
+        if (a.x > middleX && a.y < middleY) {
+            topRight.x = cv::max(a.x, topRight.x);
+            topRight.y = cv::min(a.y, topRight.y);
+        }
+        if (b.x > middleX && b.y < middleY) {
+            topRight.x = cv::max(b.x, topRight.x);
+            topRight.y = cv::min(b.y, topRight.y);
+        }
+        // Find right, bottom
+        if (a.x > middleX && a.y > middleY) {
+            rightBottom.x = cv::max(a.x, rightBottom.x);
+            rightBottom.y = cv::max(a.y, rightBottom.y);
+        }
+        if (b.x > middleX && b.y > middleY) {
+            rightBottom.x = cv::max(b.x, rightBottom.x);
+            rightBottom.y = cv::max(b.y, rightBottom.y);
+        }
+        // Find bottom, left
+        if (a.x < middleX && a.y > middleY) {
+            bottomLeft.x = cv::min(a.x, bottomLeft.x);
+            bottomLeft.y = cv::max(a.y, bottomLeft.y);
+        }
+        if (b.x < middleX && b.y > middleY) {
+            bottomLeft.x = cv::min(b.x, bottomLeft.x);
+            bottomLeft.y = cv::max(b.y, bottomLeft.y);
+        }
+    }
+//    imshow("color", color);
+
+    line(out, leftTop, topRight, Scalar(255), 3, 8);
+    line(out, topRight, Point(xMax, yMax), Scalar(255), 3, 8);
+    line(out, Point(xMax, yMax), Point(xMin, yMax), Scalar(255), 3, 8);
+    line(out, Point(xMin, yMax), leftTop, Scalar(255), 3, 8);
+
+//    imshow("HoughLinesP", out);
+
+    return out;
 }
 
 void imageSource(const string &path) {
@@ -131,8 +323,11 @@ void imageSource(const string &path) {
     Mat orig = image.clone();
     resizeToHeight(image, image, 500);
 
-    Mat gray, edged, warped;
-    preProcess(image, edged);
+    image = linesDetection(image);
+
+
+    Mat edged = image;
+//    preProcess(image, edged);
 
     // find the contours in the edged image, keeping only the
     // largest ones, and initialize the screen contour
@@ -153,13 +348,15 @@ void imageSource(const string &path) {
     sort(approx.begin(), approx.end(), compareContourAreas);
 
     // show the contour (outline) of the piece of paper
+    Mat tmp = orig.clone();
+    resizeToHeight(tmp, tmp, 500);
     for (i = 0; i < approx.size(); i++) {
-        drawContours(image, approx, i, Scalar(255, 255, 0), 2);
+        drawContours(tmp, approx, i, Scalar(255, 0, 255), 2);
         if (approx[i].size() == 4) {
             break;
         }
     }
-    imshow(path + "_contours", image);
+    imshow(path + "_contours", tmp);
     int j;
     if (i < approx.size()) {
         drawContours(image, approx, i, Scalar(0, 255, 0), 2);
@@ -169,16 +366,17 @@ void imageSource(const string &path) {
 
         // apply the four point transform to obtain a top-down
         // view of the original image
+        Mat warped;
         fourPointTransform(orig, warped, approx[i]);
         // convert the warped image to grayscale, then threshold it
         // to give it that 'black and white' paper effect
-        cvtColor(warped, warped, CV_BGR2GRAY, 1);
-        threshold(warped, warped, 127, 255, CV_THRESH_BINARY);
-        adaptiveThreshold(warped, warped, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 9, 15);
-        GaussianBlur(warped, warped, Size(3, 3), 0);
+//        cvtColor(warped, warped, CV_BGR2GRAY, 1);
+//        threshold(warped, warped, 127, 255, CV_THRESH_BINARY);
+//        adaptiveThreshold(warped, warped, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 9, 15);
+//        GaussianBlur(warped, warped, Size(3, 3), 0);
 
         resizeToHeight(warped, warped, 500);
-        imshow(path + "_res", warped);
+//        imshow(path + "_res", warped);
     }
 
 }
@@ -186,11 +384,13 @@ void imageSource(const string &path) {
 int main(int argc, char **argv) {
 
     char buffer[32];
-    for (int i = 1; i <= 3; i++) {
+    int i = 1;
+    do {
         sprintf_s(buffer, "samples/business_cards/%03d.jpg", i);
-        cout << &"Process "[i];
+        cout << i << " ";
         imageSource(buffer);
-    }
+        i++;
+    } while (i <= 10);
     waitKey(0);
     return 0;
 }
